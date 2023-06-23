ab0a46314039c9982e7b4f6c809917dcdc52afe8
Dom <domeccleston@gmail.com>
2023-06-23 15:38:28 +0100

    fix layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -43,16 +43,11 @@ export default function RootLayout({
   return (
     <html lang="en" className="[color-scheme:dark]">
       <body className="bg-gray-1100 overflow-y-scroll bg-[url('/grid.svg')] pb-36">
-        <div className="lg:pl-72">
-          <div className="mx-auto max-w-4xl space-y-8 px-2 pt-20 lg:py-8 lg:px-8">
-            <div className="rounded-lg bg-vc-border-gradient p-px shadow-lg shadow-black/20"></div>
-
+        <div className="">
+          <div className="mx-auto max-w-[1200px] space-y-8 px-2 pt-20 lg:py-8 lg:px-8">
             <div className="rounded-lg bg-vc-border-gradient p-px shadow-lg shadow-black/20">
               <div className="rounded-lg bg-black p-3.5 lg:p-6">
-                {" "}
                 <div className="space-y-9">
-                  <div className="flex justify-between"></div>
-
                   <div>{children}</div>
                 </div>
               </div>
