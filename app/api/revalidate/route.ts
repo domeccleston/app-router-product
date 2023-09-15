87402dae3527272870eee2b1c650c621858e735d
Dom <domeccleston@gmail.com>
2023-09-15 02:47:39 +0100

    revalidate

--- app/api/revalidate/route.ts
+++ app/api/revalidate/route.ts
@@ -1,16 +1,12 @@
-import { NextRequest, NextResponse } from 'next/server';
-import { revalidatePath, revalidateTag } from 'next/cache';
+import { NextRequest, NextResponse } from "next/server";
+import { revalidatePath, revalidateTag } from "next/cache";
 
 export async function GET(request: NextRequest) {
-  const path = request.nextUrl.searchParams.get('path') || '/isr/[id]';
-  const collection =
-    request.nextUrl.searchParams.get('collection') || 'collection';
-  revalidatePath(path);
-  revalidateTag(collection);
-  console.log('revalidated', path, collection);
+  revalidatePath("/");
+  console.log("revalidated");
   return NextResponse.json({
     revalidated: true,
     now: Date.now(),
-    cache: 'no-store',
+    cache: "no-store",
   });
 }
